193dc5c5de9ca1e7e0684496651297a045236454
DeMensa123 <dmensatorisova@gmail.com>
2024-09-21 18:59:07 +0200

    add config and utils

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Telegram API credentials
+API_ID = os.getenv("TELEGRAM_API_ID")
+API_HASH = os.getenv("TELEGRAM_API_HASH")
+PHONE = os.getenv("TELEGRAM_PHONE")
+TELEGRAM_SESSION = os.getenv("TELEGRAM_SESSION")
+
+# MongoDB connection details
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME")
---
+++ utils.py
@@ -0,0 +1,37 @@
+import logging
+from typing import List, Optional
+
+
+# optional use of pydantic for schema validation
+class TelegramMessage:
+    """
+    A class to represent a Telegram message, storing relevant attributes
+    such as message ID, text, timestamp, sender ID, chat ID, extracted URLs, and domains.
+    """
+
+    def __init__(
+        self,
+        channel,
+        message,
+        urls: Optional[List[str]] = None,
+        domains: Optional[List[str]] = None,
+    ) -> None:
+        self.channel_name: str = channel
+        self.message_id: int = message.id
+        self.text: str = message.text
+        self.timestamp: str = message.date.isoformat()
+        self.sender_id: int = message.sender_id
+        self.chat_id: int = message.chat_id
+        self.urls: Optional[List[str]] = urls or []
+        self.domains: Optional[List[str]] = domains or []
+
+    def to_dict(self):
+        return self.__dict__
+
+
+# Setup logging configuration
+logging.basicConfig(
+    filename="scraper.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
